08b89ed8efafaa0cb32999099e7effb529280563
mirsahib <mirsahib24@gmail.com>
2019-03-04 00:59:12 +0600

    rearranging project

--- Final_project/mysqli_connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-    DEFINE('user_name','root');
-    DEFINE('password','root');
-    DEFINE('db_name','School');
-    DEFINE('host','localhost');
-    
-    $db = @mysqli_connect(host,user_name,password,db_name)
-    OR die('Could connect with database'.mysqli_connect_error());
-?>
